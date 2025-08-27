932a462aa0bfc1bf1cc70b276c391abd8b7841a8
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-08-28 00:33:57 +0200

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,7 @@ export default function App() {
         <textarea
           defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"
           onChange={() =>
-            setContenu(texte.current.value)
-          } ref={texte}></textarea>
+            setContenu(texte.current.value)} ref={texte}></textarea>
 
         {/* Zone d'affichage */}
         <section>
