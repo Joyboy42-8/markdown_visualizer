59d55c4a67817a6cec1962ec19986f2eb7d09d8e
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2025-08-27 22:50:22 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
+import Markdown from "marked-react";
+import './App.css'
+
+export default function App() {
+  const [contenu, setContenu] = useState();
+  const texte = useRef();
+
+  // Donner le focus
+  useEffect(() => {
+    texte.current.focus();
+  }, [])
+
+  return (
+    <>
+      <main>
+        {/* Zone d'ecriture */}
+        <textarea
+          defaultValue="# `Prévisualisateur - Believemy`&#13;### Oh ! De la magie !&#13;En réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !&#13;&#13;### Cas d'utilisation&#13;* *italique*&#13;* **gras**&#13;* `monospace`&#13;* ~~barré~~&#13;* # h1&#13;* ## h2&#13;* ### h3&#13;* #### etc&#13;[Believemy](https://believemy.com)"
+          onChange={() =>
+            setContenu(texte.current.value)
+          } ref={texte}></textarea>
+
+        {/* Zone d'affichage */}
+        <section>
+          <div className="resultView">
+            <Markdown>{contenu}</Markdown>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
